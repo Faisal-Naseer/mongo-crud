98e42a43aedc81c250d824e834a648d3a92d8795
Faisal Naseer <fnaseer4@gmail.com>
2025-07-08 16:41:46 +0500

    update authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(200).json({ 'message': 'Username and password are required.' });
+    if (!user || !pwd) return res.status(401).json({ 'message': 'Username and password are required.' });
 
     const foundUser = await User.findOne({ username: user }).exec();
-    if (!foundUser) return res.sendStatus(200); //Unauthorized 
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
